7fc4dfd6e58f47702a2480106769157fb123ef35
Don Ward <10is0420@gmail.com>
2020-04-05 13:33:44 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,38 @@
+class Author
+
+attr_accessor :name, :posts, :title, :author
+
+@@post_count = 0
+
+def initialize(name)
+  @name = name
+  @posts = []
+  @author = author
+  @@post_count += 1
+end
+
+def self.post
+  @post
+end
+
+def add_post(post)
+  post.author = self
+  @posts << post
+end
+
+def add_post_by_title(title)
+  post = Post.new(title)
+  @posts << post
+  post.author = self
+  @@post_count += 1
+end
+
+def self.post_count
+  @@post_count
+end
+
+def posts
+  @posts
+end
+
+end
